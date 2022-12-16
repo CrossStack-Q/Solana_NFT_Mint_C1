5b3dec809e8e9192133f8d615ef1f4e65b53d4f1
CrossStackQ <crossstack@gmail.com>
2022-12-16 19:01:54 +0530

    ko

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,7 +44,7 @@ const Home: NextPage = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                built with @_buildspace
+                built with @_buildspace by  Anurag
               </a>
             </Box>
           </Center>
